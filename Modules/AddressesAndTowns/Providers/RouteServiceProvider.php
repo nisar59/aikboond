b696f2e03797c670c6581fc6fa9a43834cc9129e
Mahar Nisar <umair.awan@akhuwat.org.pk>
2024-02-24 11:04:35 +0500

    commit changes

--- Modules/AddressesAndTowns/Providers/RouteServiceProvider.php
+++ Modules/AddressesAndTowns/Providers/RouteServiceProvider.php
@@ -49,7 +49,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
             ->namespace($this->moduleNamespace)
-            ->group(module_path('AddressesAndTowns', '/Routes/web.php'));
+            ->group(module_path('AddressesAndTowns', 'Routes/web.php'));
     }
 
     /**
@@ -64,6 +64,6 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->middleware('api')
             ->namespace($this->moduleNamespace)
-            ->group(module_path('AddressesAndTowns', '/Routes/api.php'));
+            ->group(module_path('AddressesAndTowns', 'Routes/api.php'));
     }
 }
